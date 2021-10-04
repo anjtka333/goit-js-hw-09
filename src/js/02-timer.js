1fea842c96dfe0907e515b2630d24f34ead4a8bf
Anna Andreikiv <andreikiv.ann@gmail.com>
2021-10-04 20:36:58 +0300

    done

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -17,59 +17,59 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   timeLeft: 0,
-  onClose(selectedDates) {
-    let timeleftMS = selectedDates.getTime() - Date.now();
-    this.timeLeft = convertMs(selectedDates.getTime() - Date.now());
-    if (timeleftMS < 0) {
-      window.alert('Please choose a date in the future');
-    } else if (timeleftMS >= 0) {
-      startBtn.disabled = false;
-      return [
-        (days.innerHTML = addLeadingZero(timeLeft.days)),
-        (hours.innerHTML = addLeadingZero(timeLeft.hours)),
-        (minutes.innerHTML = addLeadingZero(timeLeft.minutes)),
-        (seconds.innerHTML = addLeadingZero(timeLeft.seconds)),
-      ];
-    }
-  },
-  onStart() {
-    setInterval(() => {
-      seconds.innerHTML = addLeadingZero(timeLeft.seconds);
-    }, 1000);
-  },
-  maxDate: new Date().fp_incr(14),
-  getDifDate(date) {
-    new Date().fp_incr(date);
-  },
+  //   onClose(selectedDates) {
+  //     let timeleftMS = selectedDates.getTime() - Date.now();
+  //     this.timeLeft = convertMs(selectedDates.getTime() - Date.now());
+  //     if (timeleftMS < 0) {
+  //       window.alert('Please choose a date in the future');
+  //     } else if (timeleftMS >= 0) {
+  //       startBtn.disabled = false;
+  //       return [
+  //         (days.innerHTML = addLeadingZero(timeLeft.days)),
+  //         (hours.innerHTML = addLeadingZero(timeLeft.hours)),
+  //         (minutes.innerHTML = addLeadingZero(timeLeft.minutes)),
+  //         (seconds.innerHTML = addLeadingZero(timeLeft.seconds)),
+  //       ];
+  //     }
+  //   },
+  //   onStart() {
+  //     setInterval(() => {
+  //       seconds.innerHTML = addLeadingZero(timeLeft.seconds);
+  //     }, 1000);
+  //   },
+  //   maxDate: new Date().fp_incr(14),
+  //   getDifDate(date) {
+  //     new Date().fp_incr(date);
+  //   },
 };
 const myInput = document.querySelector('#datetime-picker');
 flatpickr(myInput, options);
-const fp = flatpickr(myInput, {});
-const calendars = flatpickr('.calendar', {});
-myInput.addEventListener('change', function (e) {
-  console.log(e.target.value);
-  //   console.log(options.defaultDate);
-  options.onClose(new Date(e.target.value));
-  //   console.log(options.maxDate);
-});
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+// const fp = flatpickr(myInput, {});
+// const calendars = flatpickr('.calendar', {});
+// myInput.addEventListener('change', function (e) {
+//   console.log(e.target.value);
+//   //   console.log(options.defaultDate);
+//   options.onClose(new Date(e.target.value));
+//   //   console.log(options.maxDate);
+// });
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
 
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+//   // Remaining days
+//   const days = Math.floor(ms / day);
+//   // Remaining hours
+//   const hours = Math.floor((ms % day) / hour);
+//   // Remaining minutes
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   // Remaining seconds
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-  return { days, hours, minutes, seconds };
-}
-startBtn.addEventListener('click', () => {
-  options.onStart();
-});
+//   return { days, hours, minutes, seconds };
+// }
+// startBtn.addEventListener('click', () => {
+//   //   options.onStart();
+// });
